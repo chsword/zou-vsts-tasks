a54857cf1726100dc72cab3a2936f5270c4db731
Zou Jian <chsword@126.com>
2016-07-30 16:18:54 +0800

    init

---
+++ extract-xamarin-android-manifest/extract.ps1
@@ -0,0 +1,28 @@
+param(
+    [string]$pathXamarinAndroidProject,
+    [string]$configuration,
+    [string]$outputVariable
+)
+Write-Debug "Entering extract-xamarin-android-manifest task"
+Write-Debug "pathXamarinAndroidProject = $pathXamarinAndroidProject"
+Write-Debug "configuration = $configuration"
+Write-Debug "outputVariable = $outputVariable"
+
+$pathXamarinAndroidProject = $pathXamarinAndroidProject.Trim()
+$configuration = $configuration.Trim()
+$outputVariable = $outputVariable.Trim()
+
+$filePath = [System.IO.Path]::Combine($pathXamarinAndroidProject,"obj/$configuration/android/AndroidManifest.xml")
+$xml = [xml]$(Get-Content $filePath -Encoding UTF8)
+
+Set-TaskVariable -Variable "$outputVariable.PackageName" -Value $xml.manifest.package
+Set-TaskVariable -Variable "$outputVariable.ApplicationLabel" -Value $xml.manifest.application.label
+Set-TaskVariable -Variable "$outputVariable.VersionName" -Value $xml.manifest.versionName
+Set-TaskVariable -Variable "$outputVariable.VersionCode" -Value $xml.manifest.versionCode
+
+Write-Debug "output variable ($outputVariable.PackageName) = $xml.manifest.package"
+Write-Debug "output variable ($outputVariable.ApplicationLabel) = $xml.manifest.application.label"
+Write-Debug "output variable ($outputVariable.VersionName) = $xml.manifest.versionName"
+Write-Debug "output variable ($outputVariable.VersionCode) = $xml.manifest.versionCode"
+
+Write-Information "Done"
